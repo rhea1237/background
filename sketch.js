e6fa23b79903983a5a138b38dccdb426192fb734
rhea1237 <92313380+rhea1237@users.noreply.github.com>
2021-10-18 19:29:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+  background(30);
+if (keyIsDown(DOWN_ARROW)){
+  background('red')
+}
+  if (keyIsDown(UP_ARROW)){
+    background('purple')
+}
+if (keyIsDown(RIGHT_ARROW)){
+  background('BLUE')
+}
+if (keyIsDown(LEFT_ARROW)){
+  background('yellow')
+}
+}
+
+
+
+
+
